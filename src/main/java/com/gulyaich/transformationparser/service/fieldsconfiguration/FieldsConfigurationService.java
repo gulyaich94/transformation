6cc494c0a01ab0fa8a61ad6a62622c935f403a55
Anastasiia Razguliaeva <gulyaich94@gmail.com>
2021-07-19 11:26:24 +0300

    small fix

--- src/main/java/com/gulyaich/transformationparser/service/fieldsconfiguration/FieldsConfigurationService.java
+++ src/main/java/com/gulyaich/transformationparser/service/fieldsconfiguration/FieldsConfigurationService.java
@@ -4,9 +4,9 @@ package com.gulyaich.transformationparser.service.fieldsconfiguration;
 import com.gulyaich.transformationparser.config.properties.BaseFieldsConfiguration;
 import com.gulyaich.transformationparser.config.properties.BaseProperties;
 
-public interface FieldsConfigurationService<T extends BaseProperties<E>, E extends BaseFieldsConfiguration> {
+public interface FieldsConfigurationService<T extends BaseProperties<R>, R extends BaseFieldsConfiguration> {
 
-    E getConfiguration(String type);
+    R getConfiguration(String type);
 
     void setProperties(T properties);
 }
